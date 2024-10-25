04c533b2295b0e4b6a25e6e52e2e868ddf065c8b
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2024-10-25 14:16:18 +0700

    add controller code and views

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,30 @@
 class GamesController < ApplicationController
+  def index
+    @games = Game.all
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    @scores = @game.scores
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+
+    if @game.save
+      redirect_to @game, notice: "Game created successfully."
+    else
+      render :new
+    end
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:name, :date)
+  end
+
 end
--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,2 +1,26 @@
 class ScoresController < ApplicationController
+  before_action :set_game
+
+  def new
+    @score = @game.scores.build
+  end
+
+  def create
+    @score = @game.scores.build(score_params)
+    if @score.save
+      redirect_to game_path(@game), notice: "Score added successfully."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:game_id])
+  end
+
+  def score_params
+    params.require(:score).permit(:player, :points)
+  end
 end
